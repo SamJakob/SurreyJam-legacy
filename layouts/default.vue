242e8185a6eb072775e5c2ccfbe72457cee99ee7
Lawrence Bethlenfalvy <lorinc11@gmail.com>
2020-10-25 20:08:25 +0000

    Dotted background

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,8 +5,32 @@
 </template>
 
 <style lang="scss">
-html, body {
-    background: #2A2742;
+body {
+    // Colors
+    $bg-color: #2A2742;
+
+    position: relative;
+    background: $bg-color;
+    overflow-x: hidden; // Horizontal scrollbars should never happen
+
+    // Dotted background (adapted from https://codepen.io/edmundojr/pen/xOYJGw)
+    $dot-color: lighten($bg-color, 40%);
+    $dot-size: 4px;
+    $dot-space: 35px;
+    &::after {
+        content: '';
+        background:
+            linear-gradient(90deg, $bg-color ($dot-space - $dot-size), transparent 1%) center,
+            linear-gradient($bg-color ($dot-space - $dot-size), transparent 1%) center,
+            $dot-color;
+        background-size: $dot-space $dot-space;
+
+        position: absolute;
+        left: 65vw;
+        top: 70vh;
+        right: 0; bottom: 0;
+        z-index: -100; // Everything should be drawn over this.
+    }
 }
 </style>
 
