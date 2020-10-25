1c270c8914aad8c33ff2a9d138ca1ce65a49d2b1
SamJakob <me@samjakob.com>
2020-10-25 20:08:07 +0000

    Add ecosystem configuration for PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    apps: [{
+        name: "SurreyJam",
+        script: 'index.js',
+        watch: '.'
+    }],
+
+    /*
+    If you aren't deploying the project, ignore this section.
+     */
+    deploy: {
+        production: {
+            // This will need to be configured in your ~/.ssh/config
+            host: 'surreyjamhost',
+            repo: 'https://github.com/SamJakob/SurreyJam.git',
+
+            user: 'surreyjam',
+            ref: 'origin/master',
+            path: '/var/www/surreyjam',
+            'pre-deploy-local': '',
+            'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+            'pre-setup': ''
+        }
+    }
+};
